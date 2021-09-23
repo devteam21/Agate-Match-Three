b1364c16e66bb47c0b7c45a1f4bc52e524fa3309
devteam21 <hackmedev@gmail.com>
2021-09-23 22:20:56 +0700

    Bagian 2 - Setup Generator

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    #region Singleton
+    private static BoardManager _instance = null;
+    public static BoardManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<BoardManager>();
+
+            if (_instance == null)
+            {
+                Debug.LogError("Fatal Error: BoardManager not Found");
+            }
+        }
+
+        return _instance;
+    }
+}
+
+    #endregion
+
+    [Header("Board")]
+    public Vector2Int size;
+    public Vector2 offsetTile;
+    public Vector2 offsetBoard;
+
+    [Header("Tile")]
+    public List<Sprite> tileTypes = new List<Sprite>();
+    public GameObject tilePrefab;
+
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+    private TileController[,] tiles;
+
+
+
+    private void Start()
+    {
+        Vector2 tileSize = tilePrefab.GetComponent<SpriteRenderer>().size;
+        CreateBoard(tileSize);
+    }
+
+    private void CreateBoard(Vector2 tileSize)
+    {
+        tiles = new TileController[size.x, size.y];
+        Vector2 totalSize = (tileSize + offsetTile) * (size - Vector2.one);
+
+        startPosition = (Vector2)transform.position - (totalSize / 2) + offsetBoard;
+        endPosition = startPosition + totalSize;
+        
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                TileController newTile = Instantiate(tilePrefab, new Vector2(startPosition.x + ((tileSize.x + offsetTile.x) * x), startPosition.y + ((tileSize.y + offsetTile.y) * y)), tilePrefab.transform.rotation, transform).GetComponent<TileController>();
+                tiles[x, y] = newTile;
+
+                // get no tile id
+                List<int> possibleId = GetStartingPossibleIdList(x, y);
+                int newId = possibleId[Random.Range(0, possibleId.Count)];
+
+                newTile.ChangeId(newId, x, y);
+            }
+        }
+    }   
+
+    private List<int> GetStartingPossibleIdList(int x, int y)
+    {
+        List<int> possibleId = new List<int>();
+
+        for (int i = 0; i < tileTypes.Count; i++)
+        {
+            possibleId.Add(i);
+        }
+
+        if (x > 1 && tiles[x - 1, y].id == tiles[x - 2, y].id)
+        {
+            possibleId.Remove(tiles[x - 1, y].id);
+        }
+
+        if (y > 1 && tiles[x, y - 1].id == tiles[x, y - 2].id)
+        {
+            possibleId.Remove(tiles[x, y - 1].id);
+        }
+
+        return possibleId;
+    }
+
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public int id;
+
+    private BoardManager board;
+    private SpriteRenderer render;
+
+    private void Awake()
+    {
+        board = BoardManager.Instance;
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeId(int id, int x, int y)
+    {
+        render.sprite = board.tileTypes[id];
+        this.id = id;
+
+        name = "TILE_" + id + " (" + x + ", " + y + ")";
+    }
+}
